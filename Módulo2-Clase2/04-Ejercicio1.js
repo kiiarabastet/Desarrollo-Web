16ee71015a06dac018305a76085c0aca02ef9a49
kiiarabastet <kiiara.ojeda31@gmail.com>
2026-04-15 04:35:05 -0300

    Clase10

---
+++ Módulo2-Clase2/04-Ejercicio1.js
@@ -0,0 +1,20 @@
+let nombre = "Kiara";
+let edad = 24;
+let ciudad = "Exaltación de la Cruz";
+
+console.log("Hola soy", nombre)
+console.log("Mi edad es de", edad, "Años")
+console.log("Vivo en", ciudad)
+
+console.log("Hola mi nombre es " + nombre + ", tengo " + edad + " Años y vivo en " + ciudad)
+
+// camelCase
+
+let tengoQueNombrarEstaVariableEnCamelCase // siempre empieza con la primera letra en minúscula
+let otraVariable
+let kiaraojeda
+
+// ¿Cual creen que es mejor let a  Ó let primerNombre / primer_nombre ?
+let a = "Kiara"
+let primerNombre = "Kiara" // este porque si alguien revisa el código podría perder mucho tiempo en encontrar el significado y está más sujeto a cometer errores
+let primer_nombre = "Kiara"
---
+++ Módulo2-Clase2/05-Ejercicio2.js
@@ -0,0 +1,36 @@
+let indefinido
+
+console.log(indefinido)
+
+let nulo = null
+
+console.log(nulo)
+
+let numero = 3
+let string = "Hola"
+let boolean = true
+
+
+console.log("tipo de dato: " + typeof indefinido)
+console.log("tipo de dato: " + typeof nulo)
+console.log("tipo de dato: " + typeof numero)
+console.log("tipo de dato: " + typeof string)
+console.log("tipo de dato: " + typeof boolean)
+
+
+let edadKiara = 24
+let esMayor = edadKiara >= 18
+
+console.log("Es mayor de edad:", esMayor)
+
+
+
+// operaciones basicas
+
+let numero1 = 15
+let numero2 = 10
+
+console.log("El resultado de la suma es: ", (numero1 + numero2))
+console.log("El resultado de la resta es: ", (numero1 - numero2))
+console.log("El resultado de la division es: ", (numero1 / numero2))
+console.log("El resultado de la multiplicacio es: ", (numero1 * numero2))
