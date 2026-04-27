4c4b04e8976fb4b22fae04a0543801f940a252c2
kiiarabastet <kiiara.ojeda31@gmail.com>
2026-04-27 18:04:34 -0300

    Modificación1

--- Módulo2-Clase4/01-Ejemplo.js
+++ Módulo2-Clase4/01-Ejemplo.js
@@ -1,12 +1,35 @@
-//  Declaracion
+//  Declaración         
+/*
+La declaración es el momento en el que le presentas al sistema los elementos que vas a usar en tu programa (variables, funciones, clases, etc.). Básicamente, le dices a la computadora: "Oye, voy a usar un dato, resérvame un espacio en la memoria y llámalo X", o "Aquí están las instrucciones para hacer una tarea específica, guárdalas por si las necesito".
+*/  
+/*
+Asignación de memoria: Sin la declaración, la computadora no sabría cuánta memoria necesita ni cómo encontrar la información más tarde
+
+Previene errores: Establece las reglas desde el principio. Si intentas usar una variable o función que no has declarado, el programa no sabe qué hacer y colapsa (este es el clásico error de "variable no definida").
+*/
+
+// Entonces la DECLARACIÓN es:
+
 function saludar() {
-    // Aca va el codigo
+    // Aca va el código
     console.log("Hola")
 }
 
-// Como se ejecuta?
+//Ejecución
 
-// Ejecucion
+/*
+Es el momento en que la computadora (el procesador, el navegador o el intérprete) lee tu código paso a paso y realiza las acciones reales. Aquí es donde las matemáticas se calculan, la información se guarda en bases de datos y los textos se muestran en la pantalla.
+*/
+/*
+Da vida al código: Un código escrito, por muy bien declarado que esté, no es más que texto muerto hasta que se ejecuta. La ejecución es lo que convierte tus palabras en software funcional.
+
+Evalúa la lógica: Durante la ejecución es donde realmente se pone a prueba tu lógica. Es cuando el programa toma decisiones (como si un usuario puso bien o mal su contraseña) basándose en las reglas que declaraste.
+*/
+
+
+// ¿Cómo se ejecuta?
+
+// Ejecución         // Si no hacemos la declaración jamás de ejecutará
 saludar()
 
 saludar()
@@ -15,16 +38,16 @@ saludar()
 
 // console.log()
 
-// Hagamos una funcion un poquitin mas compleja
+// Hagamos una función un poquitin más compleja
 function saludarNombre(nombre) {
     console.log("Hola " + nombre)
 }
 
-saludarNombre("Santiago")
+saludarNombre("Kiara")
 
-saludarNombre("Ramon")
+saludarNombre("María")
 
-saludarNombre("Delfina")
+saludarNombre("Violeta")
 
 // ¿Como devuelvo un valor?
 function suma(numero1, numero2) {
